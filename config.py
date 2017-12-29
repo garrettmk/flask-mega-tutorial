17a98e52b0ed390be88a5b8f52493268a4e20990
garrettmk <garrettmyrick@gmail.com>
2017-12-29 10:36:55 -0800

    Completed Chapter 4, part way through Chapter 5.

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    DEBUG=True
+    FLASK_DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                             'sqlite:///' + os.path.join(basedir, 'app.db'))
