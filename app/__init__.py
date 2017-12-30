78e494b9b34e82826901190e075384081a7ba63e
Garrett Myrick <garrettmyrick@gmail.com>
2017-12-30 13:14:40 -0800

    Completed Chapter 8

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -13,4 +16,43 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not app.debug:
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        )
+    )
+
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+    mail_server = app.config['MAIL_SERVER']
+    if mail_server:
+        mail_port = app.config['MAIL_PORT']
+        mail_username = app.config['MAIL_USERNAME']
+        mail_password = app.config['MAIL_PASSWORD']
+
+        host = (mail_server, mail_port)
+        auth = (mail_username, mail_password) if mail_username or mail_password else None
+        secure = () if app.config['MAIL_USE_TLS'] else None
+
+        mail_handler = SMTPHandler(
+            mailhost=host,
+            fromaddr='no-reply@' + mail_server,
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Failure',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
--- config.py
+++ config.py
@@ -4,7 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    FLASK_DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                                              'sqlite:///' + os.path.join(basedir, 'app.db'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', None)
+    MAIL_PORT = os.environ.get('MAIL_PORT', 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', None) is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', None)
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', None)
+    ADMINS = ['garrett@mail.com']
