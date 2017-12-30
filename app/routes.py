15d3fd9ad6792e72d2cb2976694cd843481da675
garrettmk <garrettmyrick@gmail.com>
2017-12-29 17:44:15 -0800

    Started Chapter 6

--- app/routes.py
+++ app/routes.py
@@ -50,7 +50,6 @@ def logout():
     return redirect(url_for('index'))
 
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -66,3 +65,15 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post number one'},
+        {'author': user, 'body': 'Test post number two'}
+    ]
+
+    return render_template('user.html', user=user, posts=posts)
