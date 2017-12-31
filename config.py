4ea60adeabf65ad0824a8a1bbdd192bc54fc9970
Garrett Myrick <garrettmyrick@gmail.com>
2017-12-31 13:16:07 -0800

    Finished Chapter 9.

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', None)
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', None)
     ADMINS = ['garrett@mail.com']
+
+    POSTS_PER_PAGE = 3
